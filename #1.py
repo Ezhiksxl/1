a636fe9b9af2d29e7675ec45b66904a5848c89e8
Ezhiksxl <jokeryt30@gmail.com>
2023-11-10 09:41:28 +0600

    1.4

--- #1.py
+++ #1.py
@@ -4,16 +4,48 @@ conn = psycopg2.connect( host = 'localhost' , dbname = 'sultan' , user = 'postgr
 print('connected')
 
 cur = conn.cursor()
-cur.execute('select name from manager limit 3;')
+cur.execute('select name from manager;')
 usernames = [r[0] for r in cur.fetchall()]
-Found = False
-while not Found:
-    username = input('введите логин:')
-    if username in usernames:
-        print('вы есть в списке')
-        Found = True
-    else:
-        print('К сожалению мы не нашли вас в списке')
+# Found = False
+# while not Found:
+#     username = input('введите логин:')
+#     if username in usernames:
+#         print('вы есть в списке')
+#         Found = True
+#     else:
+#         print('К сожалению мы не нашли вас в списке')
+# username = input('введите логин:')
+
+username = input('')
+arr = usernames
+def binary_search(arr, x):
+    global coefizhent
+    coefizhent = x
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            print(arr)
+            return mid
+
+    print("tuta")
+    return -1
+
+result = binary_search(arr, username)
+if result != -1:
+    print(f"Имя пользователя {username} найдено в списке." , coefizhent)
+else:
+    print(f"Имя пользователя {username} не найдено в списке.")
+    
+
+
 
 conn.commit()
 cur.close()
